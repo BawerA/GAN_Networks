ced55e62397a747685173f4b32162bf25efada6b
yaboisaad <56414276+yaboisaad@users.noreply.github.com>
2020-01-08 07:25:02 +0000

    Add files via upload

---
+++ setup_environment.sh
@@ -0,0 +1,12 @@
+sudo apt-get update
+sudo apt-get install python3
+sudo apt-get install ipython
+sudo apt-get install python3-pip
+pip install tensorflow
+pip install keras
+pip install matplotlib
+pip install scikit-image
+pip install opencv-python
+pip install cv2
+pip install mahotas
+sudo apt-get install 
